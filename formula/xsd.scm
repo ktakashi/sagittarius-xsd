8fbe7c70f992b201dcb13fc12406128fcbc9c5fe
ktakashi <ktakashi@ymail.com>
2014-04-01 10:15:08 +0200

    added formula for convenience

---
+++ formula/xsd.scm
@@ -0,0 +1,11 @@
+(formula
+ (description "XSD marshal/unmarshal for Sagittarius")
+ (version "HEAD")
+ (homepage :url "https://github.com/ktakashi/sagittarius-xsd")
+ (author :name "Takashi Kato" :email "ktakashi@ymail.com")
+ (source 
+  ;;:type tar :compression gzip
+  :type zip
+  :url "https://github.com/ktakashi/sagittarius-xsd/archive/master.zip")
+
+ (install (install (directories ("src" :excludes ("CMakeLists"))))))
